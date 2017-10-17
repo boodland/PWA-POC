2b60ebff1cf44dc33e669fe1a9e13e012b72f82c
Alberto Arias <alberto@boodland.com>
2017-10-17 20:04:16 +0200

    Inject oposiciones data instead of weather data, update updateForecastCard to use the new data format and card structure

--- scripts/app.js
+++ scripts/app.js
@@ -3,31 +3,14 @@
   'use strict';
 
 
-  var injectedForecast = {
-    key: 'newyork',
-    label: 'New York, NY',
-    currently: {
-      time: 1453489481,
-      summary: 'Clear',
-      icon: 'partly-cloudy-day',
-      temperature: 52.74,
-      apparentTemperature: 74.34,
-      precipProbability: 0.20,
-      humidity: 0.77,
-      windBearing: 125,
-      windSpeed: 1.52
-    },
-    daily: {
-      data: [
-        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
-        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
-      ]
-    }
+  var injectedOposiciones = {
+    oposiciones: [
+      { key: 'policia', label: 'POLICIA NACIONAL', icon: 'policia-nacional', plazas: 934 },
+      { key: 'prisiones', label: 'FUNCIONARIO DE PRISIONES', icon: 'funcionario-prisiones', plazas: 285 },
+      { key: 'magisterio', label: 'MAGISTERIO', icon: 'magisterio', plazas: 426 }, 
+      { key: 'justicia', label: 'CUERPO DE TRAMITACION PROCESAL Y ADMINISTRATIVA', icon: 'justicia', plazas: 426 },
+      { key: 'administrativo', label: 'GENERAL ADMINISTRATIVO DE LA ADMINISTRACION DEL ESTADO', icon: 'administrativo', plazas: 1245 }
+    ]
   };
   
   var weatherAPIUrlBase = 'https://publicdata-weather.firebaseio.com/';
@@ -95,48 +78,21 @@
 
   // Updates a weather card with the latest weather forecast. If the card
   // doesn't already exist, it's cloned from the template.
-  app.updateForecastCard = function(data) {
-    var card = app.visibleCards[data.key];
-    if (!card) {
-      card = app.cardTemplate.cloneNode(true);
-      card.classList.remove('cardTemplate');
-      card.querySelector('.location').textContent = data.label;
-      card.removeAttribute('hidden');
-      app.container.appendChild(card);
-      app.visibleCards[data.key] = card;
-    }
-    card.querySelector('.description').textContent = data.currently.summary;
-    card.querySelector('.date').textContent =
-      new Date(data.currently.time * 1000);
-    card.querySelector('.current .icon').classList.add(data.currently.icon);
-    card.querySelector('.current .temperature .value').textContent =
-      Math.round(data.currently.temperature);
-    card.querySelector('.current .feels-like .value').textContent =
-      Math.round(data.currently.apparentTemperature);
-    card.querySelector('.current .precip').textContent =
-      Math.round(data.currently.precipProbability * 100) + '%';
-    card.querySelector('.current .humidity').textContent =
-      Math.round(data.currently.humidity * 100) + '%';
-    card.querySelector('.current .wind .value').textContent =
-      Math.round(data.currently.windSpeed);
-    card.querySelector('.current .wind .direction').textContent =
-      data.currently.windBearing;
-    var nextDays = card.querySelectorAll('.future .oneday');
-    var today = new Date();
-    today = today.getDay();
-    for (var i = 0; i < 7; i++) {
-      var nextDay = nextDays[i];
-      var daily = data.daily.data[i];
-      if (daily && nextDay) {
-        nextDay.querySelector('.date').textContent =
-          app.daysOfWeek[(i + today) % 7];
-        nextDay.querySelector('.icon').classList.add(daily.icon);
-        nextDay.querySelector('.temp-high .value').textContent =
-          Math.round(daily.temperatureMax);
-        nextDay.querySelector('.temp-low .value').textContent =
-          Math.round(daily.temperatureMin);
+  app.updateForecastCard = function (data) {
+    for (var i = 0; i < data.oposiciones.length; i++) {
+      var card = app.visibleCards[data.oposiciones[i].key];
+      if (!card) {
+        card = app.cardTemplate.cloneNode(true);
+        card.classList.remove('cardTemplate');
+        card.querySelector('.location').textContent = data.oposiciones[i].label;
+        card.removeAttribute('hidden');
+        app.container.appendChild(card);
+        app.visibleCards[data.oposiciones[i].key] = card;
       }
+      card.querySelector('.current .icon').classList.add(data.oposiciones[i].icon);
+      card.querySelector('.current .plazas .value').textContent = data.oposiciones[i].plazas;
     }
+    
     if (app.isLoading) {
       app.spinner.setAttribute('hidden', true);
       app.container.removeAttribute('hidden');
@@ -178,6 +134,6 @@
     });
   };
 
-  app.updateForecastCard(injectedForecast);
+  app.updateForecastCard(injectedOposiciones);
 
 })();
