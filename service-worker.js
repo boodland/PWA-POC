ed089fda0a010216f07c0e16f045516362980166
Alberto Arias <alberto@boodland.com>
2017-10-19 14:50:08 +0200

    Add full path so gh-pages can get it rigth

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
 var cacheName = 'oposicionesPWA-v1';
 var filesToCache = [
-  './',
-  './index.html',
-  './scripts/app.js',
-  './scripts/localforage.min.js',
-  './styles/ud811.css',
-  './images/administrativo.png',
-  './images/funcionario-prisiones.png',
-  './images/justicia.png',
-  './images/magisterio.png',
-  './images/policia-nacional.png',
-  './images/ic_add_white_24px.svg',
-  './images/ic_refresh_white_24px.svg'
+  'https://boodland.github.io/PWA-POC/',
+  'https://boodland.github.io/PWA-POC/index.html',
+  'https://boodland.github.io/PWA-POC/scripts/app.js',
+  'https://boodland.github.io/PWA-POC/scripts/localforage.min.js',
+  'https://boodland.github.io/PWA-POC/styles/ud811.css',
+  'https://boodland.github.io/PWA-POC/images/administrativo.png',
+  'https://boodland.github.io/PWA-POC/images/funcionario-prisiones.png',
+  'https://boodland.github.io/PWA-POC/images/justicia.png',
+  'https://boodland.github.io/PWA-POC/images/magisterio.png',
+  'https://boodland.github.io/PWA-POC/images/policia-nacional.png',
+  'https://boodland.github.io/PWA-POC/images/ic_add_white_24px.svg',
+  'https://boodland.github.io/PWA-POC/images/ic_refresh_white_24px.svg'
 ];
 
 self.addEventListener('install', function(e) {
